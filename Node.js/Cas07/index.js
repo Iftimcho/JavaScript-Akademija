2e7d23d83253defab8f5a808b292793fe21f0069
DESKTOP-UQ7FNL2\Timce <timcepop1@yahoo.com>
2021-01-18 22:18:36 +0100

    Started learning about npm. We created a node server, we created and ran our own script and we assigned a port to the server and started listening to the port (sending http request and responses). We will continue in the next lecture with the buffer and query parameters block of code in index.js file

---
+++ Node.js/Cas07/index.js
@@ -0,0 +1,54 @@
+// console.log('Hello there!');
+// console.log('Hi');
+
+// console.log(5 + 5);
+
+// console.log('Printing after a change with nodemon external package. The server will restart after saving the program.');
+
+const http = require('http');
+const { StringDecoder } = require('string_decoder');
+const util = require('util');
+const url = require('url');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000; // ke ja iskoristi porta 3000 dokolku nemame konfiguraciski fajl(env)
+
+const server = http.createServer((req, res) => {
+    // primer za endpoint localhost:3000/api/courses
+    // if(req.url === '/') {
+    //     res.write('Hello World to Home Page');
+    //     res.end();
+    // } else if (req.url === '/api/courses/nodejs') {
+    //     res.write('The response \n\n ')
+    //     res.write('Node js Course @Gen10\n\n'); // ova \n ni stava new line od koga ke se ispecati porakata. Narednoto pecatenje ke bide vo nov red.
+    //     res.write(JSON.stringify([1,2,3]) + '\n\n');
+    //     res.end('End of Message to Response');
+    // } else {
+    //     res.write('Please visit /api/courses/nodejs');
+    //     res.end();
+    // }
+
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', (chunk) => {
+        buffer += decoder.write(chunk);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+        res.writeHead(200, "OK", { "Content-type" : "text/plain" });
+        res.write('The response \n\n');
+        res.write(util.inspect(path.query) + '\n\n');
+        res.write(buffer + '\n\n');
+        res.end('End of the Message to Response');
+    });
+});
+
+// ports
+// 443 - https
+// 80, 8080 - http
+
+server.listen(PORT, () => {
+    console.log(`The HTTP Server is running/listening for request at port ${PORT}...`);
+});
