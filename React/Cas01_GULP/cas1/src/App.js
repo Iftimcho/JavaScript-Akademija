225b6a0ef5b1eaceeadc3305cf151b7229df6284
DESKTOP-UQ7FNL2\Timce <timcepop1@yahoo.com>
2021-02-15 22:39:26 +0100

    Starting with react

---
+++ React/Cas01_GULP/cas1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // const someModule = require(ime na modul) vo node toa e ES5 sintaksa
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div id="app">
+        <h2>Hello World</h2>
+      </div>
+    )
+  }
+}
