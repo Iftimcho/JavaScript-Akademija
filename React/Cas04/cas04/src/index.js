b7bfba94765944420f375ed4880db863d1c3d1e0
DESKTOP-UQ7FNL2\Timce <timcepop1@yahoo.com>
2021-02-15 22:50:22 +0100

    React State introduced with Functional and Class Components. Lifecycle methods(class) and React Hooks(Functional comps)

---
+++ React/Cas04/cas04/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './components/App';
+import { SecondApp } from './components/SecondApp';
+
+ReactDOM.render(<App />,document.getElementById('root'));
+// ReactDOM.render(
+//   <SecondApp />, document.getElementById('root')
+// );
