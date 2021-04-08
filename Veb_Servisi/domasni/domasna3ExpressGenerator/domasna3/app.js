dd0ab6b4f9ec142e478f7fccb176d280275317a5
DESKTOP-UQ7FNL2\Timce <timcepop1@yahoo.com>
2021-04-08 21:15:39 +0200

    Added database connection with error handling

--- Veb_Servisi/domasni/domasna3ExpressGenerator/domasna3/app.js
+++ Veb_Servisi/domasni/domasna3ExpressGenerator/domasna3/app.js
@@ -5,7 +5,20 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
   'mongodb://localhost:27017/carsdb',
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, err => {
+    if (err) {
+      console.log('Error occurred: ', err);
+      return; // so ova ja prekinuvame operacijata i ne se zema vo obzir pecatenjeto za uspesno povrzuvanje. return moze i da se kaci pred console.log-ot
+    }
+    console.log('Successfully connected to DB');
+
+    // Ovoj pristap e pospor od gorniot, bidejki imame razgranuvanje
+    // if (err) {
+    //   console.log('Error occurred: ', err);
+    // } else {
+    //   console.log('Successfully connected to DB');
+    // }
+  }
 );
 
 app.use(express.json());
