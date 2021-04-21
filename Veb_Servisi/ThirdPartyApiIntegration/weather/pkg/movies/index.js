a9a24f5a2bbba2653876412153414b4e5cce2d37
Iftimcho Pop-Icovski <timce1994@gmail.com>
2021-04-21 19:42:58 +0200

    Added cache for movie byTitle

--- Veb_Servisi/ThirdPartyApiIntegration/weather/pkg/movies/index.js
+++ Veb_Servisi/ThirdPartyApiIntegration/weather/pkg/movies/index.js
@@ -4,10 +4,22 @@ const MOVIES_API_KEY = config.get('movies').api_key;
 // const MOVIES_API_PREFIX = 'http://www.omdbapi.com/?apikey=';
 const MOVIES_API_PREFIX = 'http://omdbapi.com/?apikey=';
 
+const movieCache = {};
 const byTitle = async (title) => {
+    let currentTimestamp = new Date.getTime();
+    if(movieCache[title] && movieCache[title].timestamp > currentTimestamp) {
+        return movieCache[title].data;
+    }
+
     const result = await fetch(`${MOVIES_API_PREFIX}${MOVIES_API_KEY}&t=${title}`);
     const data = await result.json();
-    return data;
+    
+    movieCache[title] = {
+        data,
+        timestamp: new Date().getTime() + 60 * 1000
+    };
+
+    return movieCache[title].data;
 }
 
 const seasonCache = {};
